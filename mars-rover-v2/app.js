053a2b06ea5243e6dc53f299768341cb7f011f4b
AlexZarrella <37054157+AlexZarrella@users.noreply.github.com>
2018-06-07 14:16:01 -0400

    Add files via upload

---
+++ mars-rover-v2/app.js
@@ -0,0 +1,118 @@
+// Rover Object Goes Here
+// ======================
+var rover = {
+  direction: "N",
+  position: [0,0],
+  name: "Mars Rover"
+};
+
+// Grid
+//=======================
+var grid = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+]
+
+// ======================
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+  };
+
+console.log("Rover Direction: " + rover.direction);
+}
+// turnLeft(rover)
+
+function turnRight(rover){
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+  };
+
+console.log("Rover Direction: "+ rover.direction);
+
+}
+// turnRight(rover)
+// console.log(turnRight(rover))
+
+function moveForward(rover){
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++
+      break;
+    case 'E':
+      rover.position[1]++
+      break;
+    case 'S':
+      rover.position[0]--
+      break;
+    case 'W':
+      rover.position[1]--
+      break;
+  };
+
+console.log("Rover Direction: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+// console.log(moveForward(rover))
+
+
+
+var inst = ['f','f','l','l','r'] 
+
+function roverCommands(commands) {
+  for (var i = 0 ; i < commands.length ; i++) {
+    console.log(i)
+
+    //..............
+    switch (commands[i]){
+      case 'f':
+        moveForward(rover);
+        break;
+      case 'l':
+        turnLeft(rover);
+        break;
+      case 'r':
+        turnRight(rover);
+      break
+        ;
+    }
+ }
+}
+  // console.log(roverCommands(inst))
+
+ roverCommands(inst);
+
+
+// grid [rover.position[0]][rover.position[1]] = rover.name;}
+
+
